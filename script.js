9e1c0810f1173e82c2bc7071ead5dbe932158ff1
yanademchuk2 <116493361+yanademchuk2@users.noreply.github.com>
2025-03-20 12:36:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var fetchBtn = document.getElementById("fetchUsersBtn");
-  var loadCacheBtn = document.getElementById("loadCacheBtn");
-
-
-  fetchBtn.addEventListener("click", fetchUsers);
-
-  loadCacheBtn.addEventListener("click", loadCachedData);
-});
-
-async function fetchUsers() {
-  try {
-
-    const response = await fetch("https://reqres.in/api/users");
-    if (!response.ok) {
-      throw new Error("HTTP помилка: " + response.status);
-    }
-
-    const data = await response.json();
-
-    if (!data || !Array.isArray(data.data)) {
-      throw new Error("Помилкова структура JSON");
-    }
-
-    var users = data.data.filter(function(user) {
-      return user.first_name && user.last_name;
-    });
-
-    if (users.length === 0) {
-      throw new Error("Дані не містять коректних користувачів");
-    }
-
-    renderUsers(users);
-
-    localStorage.setItem("users", JSON.stringify(users));
-    sessionStorage.setItem("users", JSON.stringify(users));
-
-    document.querySelector(".error-message").textContent = "";
-  } catch (error) {
-
-    document.querySelector(".error-message").textContent = "Помилка: " + error.message;
-  }
+function fetchData(id) {
+	return new Promise((resolve) => {
+		const daley = Math.floor(Math.random()*2000)+1000;
+		setTimeout(() => {
+			resolve('Data for ID: ${id} (Daley: ${daley} ms)');
+		}, daley);
+	});
+};
+async function processData() {
+	try {
+		console.log("Початок паралельного виконання...");
+		const paralleResults = await Promise.all([
+			fetchData(1),
+			fetchData(2),
+			fetchData(3)
+		]);
+		console.log("Результати паралельного виконання...");
+		paralleResults.forEach(result => console.log(result));
+		console.log("Початок паралельного виконання...");
+		const ids =[];
+		for await (const result of ids.map(id => fetchData(id))) {
+			console.log(result);
+		}
+		console.log("Всі завдання виконано!");
+	}   catch (error) {
+		console.error("Сталась помилка:", error);
+	}
 }
-
-function renderUsers(users) {
-  var userList = document.querySelector(".user-list");
-  userList.innerHTML = "";
-  users.forEach(function(user) {
-    var li = document.createElement("li");
-    li.textContent = user.first_name + " " + user.last_name;
-    userList.appendChild(li);
-  });
-}
-
-function loadCachedData() {
-  var cachedUsers = localStorage.getItem("users");
-  if (cachedUsers) {
-
-    renderUsers(JSON.parse(cachedUsers));
-  } else {
-    document.querySelector(".error-message").textContent = "Немає кешованих даних.";
-  }
-}
+processData();
